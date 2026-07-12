8bf69b5976ce79f8c54de25b88585568ebe60c1d
ZentaXZ <albaraesoufi@gmail.com>
2026-07-12 17:14:02 +0200

    Replace src/main.c: GTK4 native code (gtk_application, activate callback)

--- src/main.c
+++ src/main.c
@@ -1,32 +1,24 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
-#include "utils/json_utils.h"
-#include "utils/time_utils.h"
-#include "tasks/task_manager.h"
-#include "stats/stats.h"
 
-int main(int argc, char* argv[]) {
-    gtk_init(&argc, &argv);
+static void activate(GtkApplication* app, gpointer user_data) {
+    (void)user_data;
 
-    // TODO: Initialize modules in order
-    // 1. json_utils (implicit, used by others)
-    // 2. time_utils
-    // 3. task_manager
-    // 4. stats
-    // 5. UI modules
-    // 6. control modules
-
-    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Prompt Maestro - Gestor de Tareas");
+    GtkWidget* window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Gestor de Tareas");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    GtkWidget* label = gtk_label_new("Hola, GTK4!");
+    gtk_window_set_child(GTK_WINDOW(window), label);
 
-    GtkWidget* label = gtk_label_new("Bienvenido a Prompt Maestro");
-    gtk_container_add(GTK_CONTAINER(window), label);
+    gtk_widget_set_visible(window, TRUE);
+}
 
-    gtk_widget_show_all(window);
-    gtk_main();
+int main(int argc, char** argv) {
+    GtkApplication* app = gtk_application_new("com.tuusuario.gestortareas", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-    return 0;
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
